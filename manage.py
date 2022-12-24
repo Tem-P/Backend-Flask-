790674553944f371a7e062db7e4ed7d98c0c6ac0
Shahezad <shahezad96@gmail.com>
2022-12-24 12:10:18 +0530

    remove useless code in manage.py

--- manage.py
+++ manage.py
@@ -1,27 +1,13 @@
 import os
 import unittest
 import sys
+from app.main import create_app
 
-#from flask_migrate import Migrate, MigrateCommand
-#from flask_script import Manager
-
-from app.main import create_app#, db
-
-app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
-
-app.app_context().push()
-
-#manager = Manager(app)
-
-#migrate = Migrate(app, db)
-
-#manager.add_command('db', MigrateCommand)
-
-#@manager.command
 def run():
+    app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
+    app.app_context().push()
     app.run()
 
-#@manager.command
 def test():
     """Runs the unit tests."""
     tests = unittest.TestLoader().discover('app/test', pattern='test*.py')
